b2424d921a21c803176d09f766489852010cbd93
ktay <kattie.tay@salesforce.com>
2018-01-01 15:53:37 -0800

    Fixed bug in getResponse.php related to cvs shipping

--- getResponse.php
+++ getResponse.php
@@ -4,10 +4,12 @@
      */
     header("Content-Type:text/html; charset=utf-8");
     if (isset($_REQUEST['storeid']) && $_REQUEST['storeid'] !== '') {
+        $responseType = "711";
         $storeId   = $_REQUEST['storeid'];
         $storeName = $_REQUEST['storename'];
         $storeAddress = $_REQUEST['address'];
     } else if (isset($_REQUEST['cvsspot']) && $_REQUEST['cvsspot'] !== '') {
+        $responseType = "CVS";
         $storeId   = $_REQUEST['cvsspot'];
         $storeName = $_REQUEST['name'];
         $storeAddress = $_REQUEST['addr'];
@@ -15,7 +17,13 @@
 ?>
 
 <script type="text/javascript">
-    window.opener.document.getElementById("storeid").value = "<?php echo $storeId;?>";
+    var responseType = "<?php echo $responseType;?>";
+    if (responseType === '711' && window.opener.document.getElementById("storeid") != null) {
+        window.opener.document.getElementById("storeid").value = "<?php echo $storeId;?>";
+    } else if (responseType === 'CVS') {
+        var action = window.opener.document.getElementById("mapFormId").action;
+        action.replace(/cvsspot*[&]?/, 'cvsspot=' + "<?php echo $storeId;?>");
+    }
     window.opener.document.getElementById("store_id").value = "<?php echo $storeId;?>";
     window.opener.document.getElementById("store_name").value = "<?php echo $storeName;?>";
     window.opener.document.getElementById("store_name_field").style.display = 'block';
